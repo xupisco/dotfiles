d520f19589e5e1237aee8cc11305e3363e99d7c3
Leandro Voltolino <leandro@voltolino.com>
2024-03-27 14:26:47 -0300

    hello world

---
+++ fish/alias.fish
@@ -0,0 +1,36 @@
+# System
+alias c="code ."
+alias ci="code"
+alias v="vim"
+alias e="explorer.exe"
+alias vim="nvim"
+alias l="eza -l --git --icons --no-user --group-directories-first"
+alias ll="ls -ghG --group-directories-first"
+alias r="ranger"
+alias rs="./manage.py runserver 0.0.0.0:8000"
+alias we="curl wttr.in/\?format=\"\n%l+\nNow:+%t+(%f)+/+%c%p\nSun:+%D+/+%d\n\""
+alias gs="git status"
+alias bat="batcat"
+alias !="cd ~ && vf deactivate && nvm use system"
+
+# Workon manager
+function wo
+    if test (count $argv) -ne 1
+        echo wo: Expected one arguments, the project\'s name.
+        return 1
+    end
+
+    eval "wo_"$argv[1]
+end
+
+# Projects
+alias wo_taicare="cd ~/projects/taicare && nvm use"
+alias wo_bobs="cd ~/projects/bobs_portal"
+alias wo_qr="cd ~/projects/qrhub"
+alias wo_spa="cd ~/projects/spa_reloaded"
+
+# Projects autocomplete suggestions
+complete --command wo --exclusive --condition __fish_use_subcommand --arguments taicare --description "tai.care"
+complete --command wo --exclusive --condition __fish_use_subcommand --arguments bobs --description "Bob's"
+complete --command wo --exclusive --condition __fish_use_subcommand --arguments qr --description "QRhub"
+complete --command wo --exclusive --condition __fish_use_subcommand --arguments spa --description "SPA Reloaded"
---
+++ fish/config.fish
@@ -0,0 +1,45 @@
+set -g fish_greeting
+set -gx EDITOR nvim
+set -gx EXA_ICON_SPACING 2
+
+set PATH $HOME/.local/bin $PATH
+set PATH /usr/local/go/bin $PATH
+set PATH /home/linuxbrew/.linuxbrew/bin $PATH
+
+set -Ux VIRTUALFISH_DEFAULT_PYTHON python3
+set -Ux VIRTUALFISH_HOME ~/.venvs
+
+# Pure
+set pure_enable_single_line_prompt false
+set pure_symbol_prompt '>'
+set pure_show_system_time false
+
+# Tide
+set --global tide_left_prompt_prefix ''
+set --global tide_character_icon ' '$tide_character_icon
+set --global tide_left_prompt_items pwd python git newline character
+set --global tide_right_prompt_items node time
+
+# Brew
+set -Ux HOMEBREW_AUTO_UPDATE_SECS 604800
+set -Ux HOMEBREW_NO_ENV_HINTS 1
+set -Ux HOMEBREW_NO_AUTO_UPDATE 1
+
+# FZF
+set --export FZF_DEFAULT_OPTS --height=40% --layout=reverse --border=none
+
+set -x GPG_TTY (tty)
+ssh-add -l > /dev/null || ssh-add
+
+# Aliases and Abbrs
+if [ -f $HOME/.config/fish/alias.fish ]
+    source $HOME/.config/fish/alias.fish
+end
+
+# Fun?
+function fish_greeting
+    echo "Let's get shit done!"
+end
+
+# Init starship
+starship init fish | source
